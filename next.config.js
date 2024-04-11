24601f786e0ec3d13ce1920ac065ff4649638039
Piyawud Koonmanee <piyawudkoon@hotmail.com>
2024-04-11 18:52:49 +0700

    change url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    basePath: '/lab/micro'
+    basePath: '/lab/projects/micro'
 }
 
 module.exports = nextConfig
