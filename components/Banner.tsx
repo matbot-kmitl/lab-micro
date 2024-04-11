c0c4285f97144e48a0b277dc84df690fe186a66d
Piyawud Koonmanee <piyawudkoon@hotmail.com>
2024-04-11 18:36:43 +0700

    update files

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -10,7 +10,12 @@ const Banner = () => {
     <div className="flex flex-row items-center justify-center px-20 mt-[60px] z-[20] ">
       <div className="flex flex-col  justify-center  text-center">
         <div className="justify-center flex ">
-          <Image src="/Me.png" height={300} width={300} alt="Ibrahim Memon" />
+          <Image
+            src="./Me.png"
+            height={300}
+            width={300}
+            alt="Piyawud Koonmanee"
+          />
         </div>
 
         <div className="flex flex-col gap-6 mt-6 cursor-pointer hover:-translate-y-1  transition  hover:scale-110 transition ease-in-out hover:text-transparent bg-clip-text  hover:bg-gradient-to-r from-purple-500 to-orange-400 z-[1] tracking-tighter text-7xl font-semibold text-white max-w-[600px] w-auto h-auto">
